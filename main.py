8be33c0687f5da504875439ed6eeb31cb0051314
Anton Veldanov <33134033+aveldanov@users.noreply.github.com>
2025-01-08 15:58:06 -0800

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import json
 
 app = FastAPI()
 
+# Load data from file
 with open("data.json", "r") as f:
     data = json.load(f)
+
 
 @app.get("/")
 async def read_data():
     return data
+
 
 @app.get("/{guid}")
 async def read_data_by_guid(guid: str):
